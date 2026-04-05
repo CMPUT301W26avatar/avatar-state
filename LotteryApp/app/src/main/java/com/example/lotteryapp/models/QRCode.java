eb63bea82381b089491f92d63bf3e64e9d1dd6b3
greadee <proolux@telus.net>
2026-04-05 17:34:05 -0600

    javadoc and cleanup for Model classes

--- LotteryApp/app/src/main/java/com/example/lotteryapp/models/QRCode.java
+++ LotteryApp/app/src/main/java/com/example/lotteryapp/models/QRCode.java
@@ -9,28 +9,37 @@ import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 import java.io.ByteArrayOutputStream;
 
+/**
+ * represents a qr code generator for an event that encodes a url containing the event id
+ *      this model provides utilities to generate a qr code bitmap, png byte data, and metadata for sharing or displaying event access links
+ */
+
 public class QRCode {
     private final String EVENT_ID;
     private final int WIDTH;
     private final int HEIGHT;
 
+    // 400 dp square default shape
     public QRCode(String eventId) {
         EVENT_ID = eventId;
         WIDTH = 400;
         HEIGHT = 400;
     }
 
+    // custom dimensions
     public QRCode(String eventId, int width, int height) {
         EVENT_ID = eventId;
         WIDTH = width;
         HEIGHT = height;
     }
 
+    // build the url encoded into the qr
     public String getURI() {
         return String.format("%s?event=%s",
                 "https://avatar-state-api.web.app/event", EVENT_ID);
     }
 
+    // generate a bitmap representation of the qr code for the event
     public Bitmap getBitmap() {
         BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
         try {
@@ -41,6 +50,7 @@ public class QRCode {
         }
     }
 
+    // converts the qr code bitmap into png byte data for storage or transmission
     public byte[] getPngData() {
         Bitmap bitmap = getBitmap();
         ByteArrayOutputStream bos = new ByteArrayOutputStream();
